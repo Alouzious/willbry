632e1fba17afed478a2cd7ee7506b7d20f2adf75
Alouzious <alouzious@gmail.com>
2026-05-06 12:19:45 +0300

    fix cors, manifest, navbar key

--- willbry-backend/src/main.rs
+++ willbry-backend/src/main.rs
@@ -228,11 +228,13 @@ async fn main() {
     };
 
     let cors = CorsLayer::new()
-        .allow_origin([
-            "https://willbry.vercel.app".parse::<HeaderValue>().unwrap(),
-            "http://localhost:5173".parse::<HeaderValue>().unwrap(),
-            "http://127.0.0.1:5173".parse::<HeaderValue>().unwrap(),
-        ])
+    .allow_origin([
+        "https://willbry.vercel.app".parse::<HeaderValue>().unwrap(),
+        "http://localhost:5173".parse::<HeaderValue>().unwrap(),
+        "http://localhost:5174".parse::<HeaderValue>().unwrap(),
+        "http://127.0.0.1:5173".parse::<HeaderValue>().unwrap(),
+        "http://127.0.0.1:5174".parse::<HeaderValue>().unwrap(),
+    ])
         .allow_methods(Any)
         .allow_headers(Any);
 
